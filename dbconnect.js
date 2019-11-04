69f495e286c156212bebd4e4e5ece90cb8f86066
Florian Roulet <froulet001@emi.u-bordeaux1.fr>
2019-11-04 12:23:49 +0100

    Modification de dbconnect.js pour en faire un module

--- dbconnect.js
+++ dbconnect.js
@@ -1,58 +1,49 @@
-function addElementToDB (element, collection) {
-  collection.insertOne(element, function (err, result) {
+function addElementToDB (element, collection, message) {
+  return collection.insertOne(element, function (err, result) {
     if (err) throw err
-    if (result) console.log('Element with ID \'' + element._id + '\' inserted')
+    if (result) console.log(message)
   })
 }
 
-function findElementInDB (element, collection) {
-  collection.findOne(element, function (err, result) {
+function findElementInDB (element, collection, message) {
+  return collection.findOne(element, function (err, result) {
     if (err) throw err
-    if (result) console.log(result._name)
+    if (result) {
+      console.log(message)
+    }
   })
 }
 
-function updateElementInDB (oldElement, newElement, collection) {
-  collection.updateOne(oldElement, newElement, function (err, result) {
+function updateElementInDB (oldElement, newElement, collection, message) {
+  return collection.updateOne(oldElement, newElement, function (err, result) {
     if (err) throw err
-    if (result) console.log('Element with ID \'' + oldElement._id + '\' updated')
+    if (result) console.log(message)
   })
 }
 
-function deleteElementFromDB (element, collection) {
-  collection.deleteOne(element, function (err, result) {
+function deleteElementFromDB (element, collection, message) {
+  return collection.deleteOne(element, function (err, result) {
     if (err) throw err
-    if (result) console.log('Element with ID \'' + element._id + '\' deleted')
+    if (result) console.log(message)
   })
 }
 
-function deleteCollection (collection) {
-  collection.drop(function (err, result) {
+function deleteCollection (collection, message) {
+  return collection.drop(function (err, result) {
     if (err) throw err
-    if (result) console.log('Collection deleted')
+    if (result) console.log(message)
   })
 }
 
 const MongoClient = require('mongodb').MongoClient
 const uri = 'mongodb+srv://collabcdp2019:cdp2019@cdp2019-iaivu.gcp.mongodb.net/test?retryWrites=true&w=majority'
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-client.connect(err => {
-  if (err) throw err
-  const collection = client.db('accounts').collection('logins')
-  // POST
-  const user = { _id: 'user@example.com', _name: 'user', _password: 'password' } // mongo verifies that _id is unique
-  addElementToDB(user, collection)
-  // GET
-  const userToFind = { _id: 'user@example.com' }
-  findElementInDB(userToFind, collection)
-  // UPDATE
-  const oldUser = { id: 'user@example.com' }
-  const newUser = { $set: { _name: 'newUser', _password: 'newPassword' } }
-  updateElementInDB(oldUser, newUser, collection)
-  // DELETE
-  const userToDelete = { _id: 'user@example.com' }
-  deleteElementFromDB(userToDelete, collection)
-  // /!\/!\/!\ REMOVE A WHOLE COLLECTION /!\/!\/!\
-  deleteCollection(collection)
-  client.close()
-})
+
+module.exports = {
+  addElementToDB,
+  findElementInDB,
+  updateElementInDB,
+  deleteElementFromDB,
+  deleteCollection,
+  client
+}
