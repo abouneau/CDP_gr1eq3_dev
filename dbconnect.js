f83d9d00e41423417f115c45d3f00f5578da3d08
froulet001 <florian.roulet@u-bordeaux.fr>
2019-11-05 16:34:48 +0100

    ajout de navbar.ejs, de signIn.ejs, de signUp.ejs et de logManager.js, modification de dbconnect

--- dbconnect.js
+++ dbconnect.js
@@ -1,43 +1,56 @@
 function addElementToDB (element, collection, message) {
-  return collection.insertOne(element, function (err, result) {
-    if (err) throw err
-    if (result) console.log(message)
+  collection.insertOne(element, function (err, result) {
+    if (err) console.log(err)
+    else if (result) console.log(message)
   })
 }
 
-function findElementInDB (element, collection, message) {
-  return collection.findOne(element, function (err, result) {
-    if (err) throw err
-    if (result) {
-      console.log(message)
+function findElementInDB (element, collection, message, failMessage) {
+  collection.findOne(element, function (err, result) {
+    if (err) console.log(err)
+    else {
+      if (result) console.log(message)
+      else console.log(failMessage)
+      return result
     }
   })
 }
 
 function updateElementInDB (oldElement, newElement, collection, message) {
-  return collection.updateOne(oldElement, newElement, function (err, result) {
-    if (err) throw err
-    if (result) console.log(message)
+  collection.updateOne(oldElement, newElement, function (err, result) {
+    if (err) console.log(err)
+    else if (result) console.log(message)
   })
 }
 
 function deleteElementFromDB (element, collection, message) {
-  return collection.deleteOne(element, function (err, result) {
-    if (err) throw err
-    if (result) console.log(message)
+  collection.deleteOne(element, function (err, result) {
+    if (err) console.log(err)
+    else if (result) console.log(message)
   })
 }
 
 function deleteCollection (collection, message) {
-  return collection.drop(function (err, result) {
-    if (err) throw err
-    if (result) console.log(message)
+  collection.drop(function (err, result) {
+    if (err) console.log(err)
+    else if (result) console.log(message)
   })
+}
+
+function connectToDB () {
+  client.connect(err => {
+    if (err) throw err
+  })
+}
+
+function disconnectFromDB () {
+  client.close()
 }
 
 const MongoClient = require('mongodb').MongoClient
 const uri = 'mongodb+srv://collabcdp2019:cdp2019@cdp2019-iaivu.gcp.mongodb.net/test?retryWrites=true&w=majority'
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+connectToDB()
 
 module.exports = {
   addElementToDB,
@@ -45,5 +58,6 @@ module.exports = {
   updateElementInDB,
   deleteElementFromDB,
   deleteCollection,
+  disconnectFromDB,
   client
 }
