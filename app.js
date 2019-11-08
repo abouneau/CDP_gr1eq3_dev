bcc4750ce8b6701ea294ddc1e7648770220169b3
abouneau <axel.bouneau@etu.u-bordeaux.fr>
2019-11-08 08:46:12 +0100

    modif de app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const issue = require('./routes/issueRoute')
 const log = require('./routes/logRoutes')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
 
