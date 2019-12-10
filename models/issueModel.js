e18d5705a5e134dd942682d6a53e9d78c8dab253
Nicolas Duroc <nduroc@emi.u-bordeaux1.fr>
2019-12-10 13:59:17 +0100

    US* ; TK26_SP1 doc code model

--- models/issueModel.js
+++ models/issueModel.js
@@ -1,4 +1,15 @@
 class Issue {
+  /**
+   * Construct a new issue
+   * color {String} : color for the div containing the display of the issue on the differents pages
+   * @param {String} issueID The ID of the issue (we can't have two issue with the same ID inside a project)
+   * @param {String} projectID The ID of the project containing the issue (we can't have two project with the same ID)
+   * @param {String} description The description of the issue
+   * @param {Int} priority The priority of the issue
+   * @param {Int} difficulty The difficulty of the issue
+   * @param {String} state  The state of the issue ('toDo', 'onGoing' or 'end')
+   *
+   */
   constructor (issueID, projectID, description, priority, difficulty, state) {
     this._issueID = issueID
     this._projectID = projectID
