d05400db3d7a58eeb545ec771a8f34f4823b5b6c
abouneau <axel.bouneau@etu.u-bordeaux.fr>
2019-12-10 14:21:16 +0100

    USall ; TK0SP0 - respect des conventions de nommage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 
-const issue = require('./routes/issueRoute')
+const issue = require('./routes/issueRoutes')
 const log = require('./routes/logRoutes')
 const task = require('./routes/taskRoutes')
 const test = require('./routes/testRoutes')
